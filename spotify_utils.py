bf2a7ea13e0c6e4440403f7231e5574a3c6b2ff5
michalnzr@gmail.com <michalnzr@gmail.com>
2022-09-08 12:46:46 +0000

    add aditional parameter strg_name for Routing class

--- spotify_utils.py
+++ spotify_utils.py
@@ -9,7 +9,7 @@ class Credentials:
         self.credentials = SpotifyClientCredentials(client_id= self.client_id, client_secret=self.key)
         
 class Routing:
-    def __init__(self, container_name, file_name):
+    def __init__(self, container_name, file_name, strg_name = 'spotify0storage'):
         self.container_name = container_name
         self.file_name = file_name
-        self.path = f"abfss://{self.container_name}@spotify0storage.dfs.core.windows.net/{self.file_name}"
+        self.path = f"abfss://{self.container_name}@{strg_name}.dfs.core.windows.net/{self.file_name}"
