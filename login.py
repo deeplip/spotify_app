322cc8f991c47eaf962dd1929217d37ca02d2864
michalnzr@gmail.com <michalnzr@gmail.com>
2022-09-08 12:44:07 +0000

    add login features

---
+++ login.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %run ../spotify_app/spotify_utils
+
+# COMMAND ----------
+
+def login(username, password):
+    import hashlib
+    pwhash = hashlib.sha256(password.encode()).hexdigest()
+    users_list = spark.sql(f'''
+        SELECT username FROM users.users_parquet
+            WHERE pwhash == "{pwhash}"
+    '''
+        ).rdd.flatMap(lambda x: x).collect()
+    if username in users_list:
+        params_table = 'config'
+        path = Routing('params', params_table).path
+        df = spark.read.format('parquet').load(path)
+        return df.select('playlist_name').rdd.flatMap(lambda x: x).collect()
+    raise Exception("Wrong username or password.")
